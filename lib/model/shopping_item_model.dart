e5a2b3bd32bd0f977650aa061b35b99971720188
unknown <taeho0523@gmail.com>
2024-12-30 16:08:42 +0900

    first

---
+++ lib/model/shopping_item_model.dart
@@ -0,0 +1,13 @@
+class ShoppingItemModel {
+  final String name;
+  final int quantity;
+  final bool hasBought;
+  final bool spicy;
+
+  ShoppingItemModel({
+    required this.name,
+    required this.quantity,
+    required this.hasBought,
+    required this.spicy,
+  });
+}
